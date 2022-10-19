f7c43ca61aca9ba0f838b008c87f02908ef7e10e
Muideen27 <ilorimuideen0000@gmail.com>
2022-10-19 15:43:59 -0700

    98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 -> prints all natural numbers from n to 98
+ * followed by a new line
+ * numbers must be separated by a comma,
+ * @n: number to begin counting
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+	printf("%d\n", n);
+	}
+}
