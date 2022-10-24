5dd6ab8e4b1b2f3f411148e6910ca55027279af8
Muideen27 <ilorimuideen0000@gmail.com>
2022-10-24 11:34:28 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.s
@@ -0,0 +1,107 @@
+	.file	"100-atoi.c"
+	.text
+	.globl	_atoi
+	.type	_atoi, @function
+_atoi:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	$0, -16(%rbp)
+	movl	$0, -12(%rbp)
+	movl	$1, -8(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L9:
+	movl	-16(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$45, %al
+	jne	.L4
+	negl	-8(%rbp)
+	jmp	.L4
+.L6:
+	movl	$1, -4(%rbp)
+	movl	-12(%rbp), %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	movl	%eax, %ecx
+	movl	-16(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	addl	%ecx, %eax
+	subl	$48, %eax
+	movl	%eax, -12(%rbp)
+	addl	$1, -16(%rbp)
+.L4:
+	movl	-16(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$47, %al
+	jle	.L5
+	movl	-16(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$57, %al
+	jle	.L6
+.L5:
+	cmpl	$1, -4(%rbp)
+	je	.L11
+	addl	$1, -16(%rbp)
+.L2:
+	movl	-16(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L9
+	jmp	.L8
+.L11:
+	nop
+.L8:
+	movl	-8(%rbp), %eax
+	movl	-12(%rbp), %edx
+	imull	%edx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_atoi, .-_atoi
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
