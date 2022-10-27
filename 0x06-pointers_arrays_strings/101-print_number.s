b3cd9befb35e3b7dd0d31d33b7be6bb6c3cccc3b
Muideen27 <ilorimuideen0000@gmail.com>
2022-10-27 03:24:09 -0700

    Numbers have life; they're not just symbols on paper updated

--- 0x06-pointers_arrays_strings/101-print_number.s
+++ 0x06-pointers_arrays_strings/101-print_number.s
@@ -11,355 +11,41 @@ print_number:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$96, %rsp
-	movl	%edi, -84(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1152921505, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$28, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -76(%rbp)
-	movl	-76(%rbp), %eax
-	movl	%eax, -36(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1441151881, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$25, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -72(%rbp)
-	movl	-72(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -72(%rbp)
-	movl	-36(%rbp), %edx
-	movl	-72(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -32(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1801439851, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$22, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -68(%rbp)
-	movl	-68(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -68(%rbp)
-	movl	-32(%rbp), %edx
-	movl	-68(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -28(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1125899907, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$18, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -64(%rbp)
-	movl	-64(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -64(%rbp)
-	movl	-28(%rbp), %edx
-	movl	-64(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -24(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$351843721, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$13, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -60(%rbp)
-	movl	-60(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -60(%rbp)
-	movl	-24(%rbp), %edx
-	movl	-60(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -20(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1759218605, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$12, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -56(%rbp)
-	movl	-56(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -56(%rbp)
-	movl	-20(%rbp), %edx
-	movl	-56(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -16(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$274877907, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$6, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -52(%rbp)
-	movl	-52(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -52(%rbp)
-	movl	-16(%rbp), %edx
-	movl	-52(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -12(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1374389535, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$5, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -48(%rbp)
-	movl	-48(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -48(%rbp)
-	movl	-12(%rbp), %edx
-	movl	-48(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -8(%rbp)
-	movl	-84(%rbp), %eax
-	movslq	%eax, %rdx
-	imulq	$1717986919, %rdx, %rdx
-	shrq	$32, %rdx
-	sarl	$2, %edx
-	sarl	$31, %eax
-	subl	%eax, %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	movl	%eax, -44(%rbp)
-	movl	-44(%rbp), %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -44(%rbp)
-	movl	-8(%rbp), %edx
-	movl	-44(%rbp), %eax
-	addl	%edx, %eax
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	-20(%rbp), %eax
 	movl	%eax, -4(%rbp)
-	movl	-84(%rbp), %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	movl	%eax, %ecx
-	sarl	$2, %ecx
-	movl	%edx, %eax
-	sarl	$31, %eax
-	subl	%eax, %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %eax
-	movl	%eax, -40(%rbp)
-	cmpl	$0, -84(%rbp)
+	cmpl	$0, -20(%rbp)
 	jns	.L2
 	movl	$45, %edi
 	call	_putchar@PLT
-	negl	-76(%rbp)
-	negl	-72(%rbp)
-	negl	-68(%rbp)
-	negl	-64(%rbp)
-	negl	-60(%rbp)
-	negl	-56(%rbp)
-	negl	-52(%rbp)
-	negl	-48(%rbp)
-	negl	-44(%rbp)
-	negl	-40(%rbp)
+	negl	-4(%rbp)
 .L2:
-	cmpl	$0, -36(%rbp)
-	je	.L3
-	movl	-76(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
+	cmpl	$9, -4(%rbp)
+	jbe	.L3
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	movl	$3435973837, %eax
+	imulq	%rdx, %rax
+	shrq	$32, %rax
+	shrl	$3, %eax
 	movl	%eax, %edi
-	call	_putchar@PLT
+	call	print_number
 .L3:
-	cmpl	$0, -32(%rbp)
-	je	.L4
-	movl	-72(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L4:
-	cmpl	$0, -28(%rbp)
-	je	.L5
-	movl	-68(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L5:
-	cmpl	$0, -24(%rbp)
-	je	.L6
-	movl	-64(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L6:
-	cmpl	$0, -20(%rbp)
-	je	.L7
-	movl	-60(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L7:
-	cmpl	$0, -16(%rbp)
-	je	.L8
-	movl	-56(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L8:
-	cmpl	$0, -12(%rbp)
-	je	.L9
-	movl	-52(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L9:
-	cmpl	$0, -8(%rbp)
-	je	.L10
-	movl	-48(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L10:
-	cmpl	$0, -4(%rbp)
-	je	.L11
-	movl	-44(%rbp), %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-.L11:
-	movl	-40(%rbp), %eax
+	movl	-4(%rbp), %ecx
+	movl	%ecx, %edx
+	movl	$3435973837, %eax
+	imulq	%rdx, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	shrl	$3, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	movl	%edx, %eax
 	addl	$48, %eax
 	movsbl	%al, %eax
 	movl	%eax, %edi
