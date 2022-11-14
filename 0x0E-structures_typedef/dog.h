eb8b5f3011982805fc6f60a116bf5c132375d691
Muideen27 <ilorimuideen0000@gmail.com>
2022-11-14 06:14:59 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - A dog
- * @name: Name of dog
- * @age: Age of dog
- * @owner: Owner of dog
+ * struct dog - creates a struct for dogs' information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: Longer description
  */
-typedef struct
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-dog;
-	/**
-	 * dog_t - Typedef for struct dog
-	 */
+};
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
