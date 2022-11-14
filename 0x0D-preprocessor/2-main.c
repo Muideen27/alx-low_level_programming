60776138bdb293d90f4516dd7bfdbd2c50145f58
Muideen27 <ilorimuideen0000@gmail.com>
2022-11-14 07:49:28 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from fll new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n",  (__FILE__));
+	return (0);
+}
