aae926391e2904b70d3acf5640967d4a7c03bfff
Muideen27 <ilorimuideen0000@gmail.com>
2022-12-07 10:36:55 -0800

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,11 +7,12 @@
 #include <sys/stat.h>
 
 /**
- * create_file - Write a script that creates a file 
+ * create_file - Write a script that creates a file
  * @filename: pointer to name of the file to create
- * Returns: 1 on success, -1 on failure
- */
-/* if filename is NULL return -1
+ * @text_content: A pointer to a string to write
+ * to the file.
+ * Return: 1 on success, -1 on failure
+ * if filename is NULL return -1
  * if text_content is NULL create an empty file
  */
 
@@ -22,17 +23,17 @@ int create_file(const char *filename, char *text_content)
 	char *ptr;
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1);
+	if (file == -1)
 	{
-		return(-1);
+		return (-1);
 	}
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 	if (text_content != NULL)
 		for (inlen = 0, ptr = text_content; *ptr; ptr++)
 			inlen++;
-	length =write(file, text_content, inlen);
+	length = write(file, text_content, inlen);
 	if (close(file) == -1 || inlen != length)
-		return(-1);
-	return(1);
+		return (-1);
+	return (1);
 }
