7b3a42e70da27292a28234d5767d8ee5a73fdc7a
Muideen27 <ilorimuideen0000@gmail.com>
2022-12-24 22:20:15 -0800

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,20 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * int_index - searches for an integer.
- * @array: to be iterated.
- * @size: of the array.
- * @cmp: pointer to function that compares values.
- * Return: the index of the first element for which the cmp is != 0.
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if (array == NULL)
 		return (-1);
-
 	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
@@ -22,6 +24,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
-
 	return (-1);
 }
