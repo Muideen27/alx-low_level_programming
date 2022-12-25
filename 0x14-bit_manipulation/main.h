357fce47603a1e4341a5a00546a370d0eb2faf3a
Muideen27 <ilorimuideen0000@gmail.com>
2022-12-24 23:34:31 -0800

    bit manipulation header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,19 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Muideen A Ilori
- */
-
-#include <stddef.h>
-#include <stdarg.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-/*Prototype*/
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -21,7 +11,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
 
-/*MAIN_H*/
-#endif
+#endif /* MAIN_H */
